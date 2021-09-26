74246fbb8d24bfd6e752e74769ef5af6acf9e965
Nikola <nikolavukic333@gmail.com>
2021-09-26 14:54:44 +0200

    Final changes

---
+++ validation.js
@@ -0,0 +1,16 @@
+function validate() {
+
+    const email = document.getElementById("email").value;
+    const valid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    if(email.match(valid)) {
+        document.getElementById("email").value = "";
+        document.getElementById("error").innerHTML = "";
+        document.getElementById("email").style.border = "1px solid hsl(0, 36%, 70%)";
+        document.getElementById("error-icon").style.display = "none";
+    } else {
+        document.getElementById("error").innerHTML = "Please provide a valid email adress";
+        document.getElementById("email").style.border = "2px solid hsl(0, 93%, 68%)";
+        document.getElementById("error-icon").style.display = "inline";
+    }
+}
