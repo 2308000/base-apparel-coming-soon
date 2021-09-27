1ca9dce554bced0abde14127ea91c53d76fa7c53
Nikola <nikolavukic333@gmail.com>
2021-09-27 11:50:25 +0200

    Added email feedback upon sending

--- validation.js
+++ validation.js
@@ -5,7 +5,8 @@ function validate() {
 
     if(email.match(valid)) {
         document.getElementById("email").value = "";
-        document.getElementById("error").innerHTML = "";
+        document.getElementById("error").innerHTML = "Email has been sent";
+        document.getElementById("error").style.color = "hsl(0, 6%, 24%)";
         document.getElementById("email").style.border = "1px solid hsl(0, 36%, 70%)";
         document.getElementById("error-icon").style.display = "none";
     } else {
